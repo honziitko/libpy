0e389713cd33bb5db15ed21502b4af2d77dc596e
Honzitko <236162629+honziitko@users.noreply.github.com>
2025-11-22 09:29:49 +0100

    Add stdbool.h

--- example.py
+++ example.py
@@ -1,7 +1,8 @@
 import libpy
+from libpy.stdbool_h import *
 from libpy.string_h import *
 
-libpy.set_address_enabled(True)
+libpy.set_address_enabled(true)
 
 for i in range(1_000):
     print(i)
---
+++ libpy/stdbool_h.py
@@ -0,0 +1,26 @@
+"""
+NAME
+    stdbool_h - Booleans for the Python language
+
+SYNOPSIS
+    #define bool _Bool
+    #define true 1
+    #define false 0
+    #define __bool_true_false_are_defined 1
+
+DESCRIPTION
+    Provides the boolean type.
+
+NOTES
+    Deprecated in Python 3.12.
+
+SEE ALSO
+    Full documentation <https://cppreference.com/w/c/header/stdbool.html>
+
+    iso646_h(7)
+"""
+
+_Bool = bool
+true = True
+false = False
+__bool_true_false_are_defined = true
